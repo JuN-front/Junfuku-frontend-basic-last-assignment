e1d9a22c62ba0adaa1cfa823f65f16ca7bb52b18
Junsei <120575865+JuN-front@users.noreply.github.com>
2023-02-26 10:06:15 +0900

    Delete atoms-button.stories.js

--- src/stories/atoms-button.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Example from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-
-export default { title: "Atoms/Button" }
-
-export const Example = () => Item_Example;
